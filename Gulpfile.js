22ef0c7db9ae385dc4def2f3b0d45267de15d64a
Christian Schmidt <christian@parafrasis.net>
2018-07-11 16:33:25 -0400

    initial project version

---
+++ Gulpfile.js
@@ -0,0 +1,92 @@
+'use strict';
+// -----------------------------------------------------------------------------
+// Gulpfile.js
+// Configuración para tests de Gulp con diferentes componentes npm.
+// GULP 4
+// https://github.com/gulpjs/gulp/blob/4.0/README.md#use-last-javascript-version-in-your-gulpfile
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+// Dependencias para el desarrollo (npm install xxx@yyy --dev)
+// -----------------------------------------------------------------------------
+const gulp = require('gulp');
+// var data = require('gulp-data');
+// var markdown = require('gulp-markdown');
+const render = require('gulp-nunjucks-render');
+const sass = require('gulp-sass');
+const sync = require('browser-sync').create();
+
+
+// -----------------------------------------------------------------------------
+// Markdown
+// Trabajar con gulp-rename, si no, la extensión del resultado
+// sigue siendo "md".
+// Ver: https://github.com/sindresorhus/gulp-markdown/issues/1
+// -----------------------------------------------------------------------------
+
+// gulp.task('markdown', function(){
+// 	return gulp.src('src/md/*.md')
+// 		.pipe(markdown())
+// 		.pipe(gulp.dest('src/nunjucks/templates/md2html'));
+// });
+
+// -----------------------------------------------------------------------------
+// Templating
+// -----------------------------------------------------------------------------
+
+gulp.task('nunjucks', function(done) {
+    return gulp.src('src/nunjucks/pages/*.njk')
+        .pipe(render({
+            path: ['src/nunjucks/pages/', 'src/nunjucks/templates/', 'src/nunjucks/templates/partials/', 'src/nunjucks/macros/']
+        }))
+			.pipe(gulp.dest('dist/'));
+	done();
+});
+
+// -----------------------------------------------------------------------------
+// Sass
+// -----------------------------------------------------------------------------
+gulp.task('sass', function(done) {
+	return gulp.src('src/sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+			.pipe(gulp.dest('dist/css'));
+	done();
+});
+
+
+// -----------------------------------------------------------------------------
+// Static server
+// https://github.com/gulpjs/gulp/blob/master/docs/recipes/minimal-browsersync-setup-with-gulp4.md
+// -----------------------------------------------------------------------------
+function reload(done) {
+	sync.reload();
+	done();
+}
+function serve(done) {
+	sync.init({
+		server: {
+			baseDir: 'dist'
+		}
+	});
+	done();
+}
+
+// -----------------------------------------------------------------------------
+// Watchers
+// https://stackoverflow.com/questions/39665773/gulp-error-watch-task-has-to-be-a-function
+// -----------------------------------------------------------------------------
+gulp.task('watch', function(done) {
+    gulp.watch(['src/sass/**/*.scss', 'src/nunjucks/**/*.njk'],	
+    gulp.series('sass', 'nunjucks', reload));
+	done();
+});
+
+// -----------------------------------------------------------------------------
+// Default task
+// -----------------------------------------------------------------------------
+gulp.task('default', gulp.series('sass', 'nunjucks', serve, 'watch'));
+
+// gulp.task('default', ['sass', 'markdown', 'nunjucks', 'watch', 'browser-sync']);
+// gulp.task('default', ['sass', 'nunjucks', 'watch', 'browser-sync']);
+// gulp.task('default', ['sass', 'nunjucks', 'watch', 'browse']);
+// gulp.task('default', gulp.series('nunjucks', 'sass', gulp.parallel(serve,'watch')));
---
+++ README.md
@@ -0,0 +1,3 @@
+# Gulp-Nunjucks
+
+This is a simple template to work with Nunjucks using Gulp 4.
